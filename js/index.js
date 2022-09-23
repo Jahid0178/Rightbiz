93d44326435a62dfebaa50a2242a2241490ce4e9
Jahid0178 <jahidulhaque767@gmail.com>
2022-09-23 18:12:41 +0600

    added accordion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-// const title = document.querySelectorAll(".title");
-// title.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     const
-//   });
-// });
+const title = document.querySelectorAll(".title");
+
+for (let i = 0; i < title.length; i++) {
+  title[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
